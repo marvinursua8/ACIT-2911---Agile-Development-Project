a5275d698df1743fc5e3eda5179a0693f7ec61f9
marvinursua8 <mursua1@my.bcit.ca>
2026-04-29 18:26:58 -0700

    initial starting flask app

---
+++ routes/api/routes.py
@@ -0,0 +1,9 @@
+from flask import current_app, render_template, Blueprint
+
+
+app1 = Blueprint("home", __name__)
+
+
+@app1.route('/index')
+def index():
+    return render_template('index.html') # test render.
