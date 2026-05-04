01be7d16bfbe924538bc6a2460206b2bd2b2729f
marvinursua8 <mursua1@my.bcit.ca>
2026-05-04 14:43:43 -0700

    lmao

--- web/__init__.py
+++ web/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
-# 1. Add the db_url parser
+from flask_login import LoginManager, login_user, logout_user, login_required
 from playhouse.db_url import connect 
 from .database import db
-from .models import User, Animal, Image
+from .models import User, Animal, Image, Admin
 from . import config
 
-# 2. Corrected URL (No brackets, no pgbouncer flag)
 DATABASE_URL="postgresql://postgres.xrygojfhrciovwsakyxk:apples7894561230qweA!@aws-1-us-east-1.pooler.supabase.com:6543/postgres"
 
 def create_app():
     app = Flask(__name__)
     from .api.routes import app1
+    login = LoginManager(app)
 
-    # --- DELETE THE OLD SQLITE CODE ---
-    # db_path = Path(config.DATABASE_PATH)
-    # db_path.parent.mkdir(exist_ok=True)
-    # db.init(str(db_path))
+    @login.user_loader
+    def load_user(admin_id):
+        return Admin.get_or_none(Admin.id == admin_id)
 
-    # --- ADD THE NEW SUPABASE CODE ---
     supabase_db = connect(DATABASE_URL)
     db.initialize(supabase_db)
     # ---------------------------------
@@ -34,6 +32,6 @@ def create_app():
     app.register_blueprint(app1)
 
     with db:
-        db.create_tables([User, Animal, Image], safe=True)
+        db.create_tables([User, Animal, Image, Admin], safe=True)
 
     return app
