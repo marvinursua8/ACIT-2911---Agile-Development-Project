edb7577058a6cf0f6bbafcf3c7fd862544405134
marvinursua8 <mursua1@my.bcit.ca>
2026-05-05 11:57:47 -0700

    added login template

--- web/api/routes.py
+++ web/api/routes.py
@@ -98,34 +98,34 @@ def view_pets():
         return jsonify(animal_list), 200
     
 
-@app1.route('/login', methods=['GET', 'POST']) # Add methods!
-def login():
-    form = LoginForm()
-    
-    # This checks if it's a POST request and the CSRF token is valid
-    if form.validate_on_submit(): 
-        # Do your login logic here (checking the database, etc.)
-        pass
-        
-    return render_template('adminlogin.html', title='Sign In', form=form)
-
-
-# @app1.route('/login', methods=['GET', 'POST'])
+# @app1.route('/login', methods=['GET', 'POST']) # Add methods!
 # def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-    
 #     form = LoginForm()
-#     if form.validate_on_submit():
-#         admin = Admin.get_or_none(Admin.username == form.username.data)
-        
-#         if admin is None or not admin.check_password(form.password.data):
-#             flash('Invalid username or password')
-#             return redirect(url_for('login'))
-            
-#         # Log the admin in! 
-#         login_user(admin, remember=form.remember_me.data)
-#         return redirect(url_for('index'))
     
-#     print("authenticated!")
-#     return render_template('/')
+#     # This checks if it's a POST request and the CSRF token is valid
+#     if form.validate_on_submit(): 
+#         # Do your login logic here (checking the database, etc.)
+#         pass
+        
+#     return render_template('adminlogin.html', title='Sign In', form=form)
+
+
+@app1.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        admin = Admin.get_or_none(Admin.username == form.username.data)
+        
+        if admin is None or not admin.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+            
+        # Log the admin in! 
+        login_user(admin, remember=form.remember_me.data)
+        return redirect(url_for('index.html'))
+    
+    print("authenticated!")
+    return render_template('adminlogin.html', title='Sign In', form=form)
