3e9a6b7d9461c86d1f67aa3f7d52a992491139e1
Keagan <keacopey@gmail.com>
2026-05-20 21:04:14 -0700

    Seed db with basically presentation ready data

---
+++ supabase/images_expired.sql
@@ -0,0 +1,6 @@
+-- The images associated with these pets expired which we obviously don't want in our presentable app
+-- But I just moved them here you can add them back easily
+    (149, 'DYLAN2.0', 'HUMAN', 'DEFINITELY', 'male', 67, 'large', 'WHITE', 'Not house trained', '6767676767676767676767676767667', false),
+	(154, 'Nilla', 'Cat', 'American Short Hair', 'female', 10, 'small', 'Tortoiseshell', 'House trained', 'Nilla bean :)', true),
+    (125, 'Neo', 'Cat', 'Cat', 'male', 7, 'small', 'White and gray', 'House trained', 'Neo is a sweet, quiet, and independent boy, except when he wants the occasional pet or when he''s hungry! He is very laid-back and appreciates his space. He would do well in a quiet home preferably as the only pet in the home.', false),
+    (108, 'Dylan', 'Human', 'yes', 'male', 18, 'large', 'yeah', 'Not house trained', 'a loveable guy', true),
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -166,22 +166,16 @@ INSERT INTO "public"."admin" ("id", "username", "password_hash") VALUES
 --
 
 INSERT INTO "public"."animals" ("id", "name", "species", "breed", "gender", "age", "size", "color", "house_trained", "description", "adopted") VALUES
-	(1, 'Robin', 'Dog', 'Dachshund', 'Male', 9, 'medium', 'brown', 'House trained', 'Marvin''s dog yaaaaaaaaaaay', true),
-	(108, 'Dylan', 'Human', 'yes', 'male', 18, 'large', 'yeah', 'Not house trained', 'a loveable guy', true),
-	(55, 'Roxy', 'Goby', 'Unknown', 'female', 2, 'small', 'Brown', 'Not house trained', 'Glop glop', true),
+	(1, 'Robin', 'Dog', 'Dachshund', 'Male', 9, 'medium', 'brown', 'House trained', 'Marvin''s dog yaaaaaaaaaaay', false),
+	(55, 'Roxy', 'Goby', 'Unknown', 'female', 2, 'small', 'Brown', 'Not house trained', 'Glop glop', false),
 	(3, 'Fifi', 'Dog', 'Pomeranian', 'Female', 5, 'small', 'white', 'House trained', 'awwwwww', false),
 	(56, 'Sunny', 'Sun bear', 'Unknown', 'male', 5, 'large', 'Black', 'Not house trained', 'Crazy long tongue', false),
 	(61, 'Sunbringer', 'Cat', 'Khao Manee', 'female', 8, 'small', 'White', 'House trained', 'First-generation Void Hunter', false),
-	(125, 'Neo', 'Cat', 'Cat', 'male', 7, 'small', 'White and gray', 'House trained', 'Neo is a sweet, quiet, and independent boy, except when he wants the occasional pet or when he''s hungry! He is very laid-back and appreciates his space. He would do well in a quiet home preferably as the only pet in the home.', false),
 	(127, 'Aqua', 'Cat', 'Tabby', 'female', 3, 'small', 'Light Brown', 'House trained', 'Very warm and soft when cuddling', false),
-	(128, 'Felix', 'Cat', '???', 'male', 5, 'small', 'White', 'House trained', 'Likes to sneak around', false),
+	(128, 'Felix', 'Cat', 'Unknown', 'male', 5, 'small', 'White', 'House trained', 'Likes to sneak around', false),
 	(132, 'Gerald II', 'Cat', 'British Shorthair', 'Male', 11, 'small', 'Gray', 'Not house trained', 'Adopt him or else', false),
-	(149, 'DYLAN2.0', 'HUMAN', 'DEFINITELY', 'male', 67, 'large', 'WHITE', 'Not house trained', '6767676767676767676767676767667', false),
-	(154, 'Nilla', 'Cat', 'American Short Hair', 'female', 10, 'small', 'Tortoiseshell', 'House trained', 'Nilla bean :)', true),
-	(130, 'Joe', 'Dog', 'Unknown', 'Male', 4, 'large', 'brown', 'House trained', 'Woof', true),
-	(2, 'Rocco', 'Dog', 'German Shepard', 'Male', 7, 'large', 'light brown', 'House trained', 'Will protecc', true),
-	(174, 'froo', 'frog', 'tree frog', 'male', 3, 'medium', 'green', 'House trained', 'slimey', false),
-	(212, 'aa', 'aa', 'aa', 'female', 3, 'small', 'aa', 'House trained', 'aa', false);
+	(130, 'Joe', 'Dog', 'Unknown', 'Male', 4, 'large', 'brown', 'House trained', 'Woof', false),
+	(2, 'Rocco', 'Dog', 'German Shepard', 'Male', 7, 'large', 'light brown', 'House trained', 'Will protecc', false);
 
 
 --
@@ -271,10 +265,6 @@ INSERT INTO "public"."images" ("id", "url", "animal_id", "is_primary") VALUES
 	(263, 'https://images.weserv.nl/?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F16691263%2Fpexels-photo-16691263.jpeg&w=500&fit=cover', 132, false),
 	(264, 'https://images.weserv.nl/?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F35279222%2Fpexels-photo-35279222.jpeg&w=500&fit=cover', 132, false),
 	(265, 'https://images.weserv.nl/?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F13773025%2Fpexels-photo-13773025.jpeg&w=500&fit=cover', 132, false),
-	(266, 'https://cdn.discordapp.com/attachments/1498433978372194467/1504171661010796594/jail.gif?ex=6a07fec6&is=6a06ad46&hm=70a38e5870e32cbc76e4cc318498ac969b0d0b2d05727e3805b5cbcb5da5933e&', 149, true),
-	(274, 'https://cdn.discordapp.com/attachments/419741937868734475/1505052537261133834/IMG_5159.jpg?ex=6a0938e7&is=6a07e767&hm=7667d92c306504c6f1ee15c26d5f75686a9577deae8a31af02f359ca6cf82a9d&', 154, true),
-	(312, 'https://images.weserv.nl/?url=https%3A%2F%2Fi.natgeofe.com%2Fk%2F8fa25ea4-6409-47fb-b3cc-4af8e0dc9616%2Fred-eyed-tree-frog-on-leaves-3-2_3x4.jpg&w=500&fit=cover', 174, true),
-	(313, 'https://www.fakesite.com/fails.jpg', 130, false),
 	(1, 'https://images.weserv.nl/?url=https%3A%2F%2Fxrygojfhrciovwsakyxk.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fimages%2FIMG_2709.jpg&w=500&fit=cover', 1, true),
 	(2, 'https://images.weserv.nl/?url=https%3A%2F%2Fxrygojfhrciovwsakyxk.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fimages%2FIMG_2647.jpg&w=500&fit=cover', 1, false),
 	(3, 'https://images.weserv.nl/?url=https%3A%2F%2Fxrygojfhrciovwsakyxk.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fimages%2Fbest-guard-dogs-1650302456.avif&w=500&fit=cover', 2, true),
@@ -285,12 +275,8 @@ INSERT INTO "public"."images" ("id", "url", "animal_id", "is_primary") VALUES
 	(154, 'https://images.weserv.nl/?url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F9144981%2Fpexels-photo-9144981.jpeg&w=500&fit=cover', 130, false),
 	(155, 'https://images.weserv.nl/?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F66%2FMastiff_-_English_Mastiff.jpg&w=500&fit=cover', 130, false),
 	(156, 'https://shorturl.at/ORkbJ', 61, true),
-	(226, 'https://cdn.discordapp.com/attachments/1487145865419165716/1504592464789508227/IMG_0340.jpg?ex=6a078c6d&is=6a063aed&hm=c7bbc7b546327bbe6fc2c519b534670b478e748a8e85308532ff5aac6bca9a53&', 108, true),
-	(255, 'https://cdn.discordapp.com/attachments/1468078954438201437/1503491155420250263/image0.jpg?ex=6a077f40&is=6a062dc0&hm=6b088d3162774992a59c9f1ee3d7b08353a30f578138f48983ad3e55546f3dd7&', 125, true),
 	(257, 'https://images.weserv.nl/?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fc7%2FTabby_cat_with_blue_eyes-3336579.jpg%2F960px-Tabby_cat_with_blue_eyes-3336579.jpg%3Futm_source%3Dcommons.wikimedia.org%26utm_campaign%3Dimageinfo%26utm_content%3Dthumbnail&w=500&fit=cover', 127, true),
-	(258, 'https://images.weserv.nl/?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe5%2FBlue-eyed_domestic_cat_%2528Felis_silvestris_catus%2529.jpg%2F960px-Blue-eyed_domestic_cat_%2528Felis_silvestris_catus%2529.jpg%3Futm_source%3Dcommons.wikimedia.org%26utm_campaign%3Dimageinfo%26utm_content%3Dthumbnail&w=500&fit=cover', 128, true),
-	(367, 'rsvds', 212, true),
-	(368, '', 212, false);
+	(258, 'https://images.weserv.nl/?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe5%2FBlue-eyed_domestic_cat_%2528Felis_silvestris_catus%2529.jpg%2F960px-Blue-eyed_domestic_cat_%2528Felis_silvestris_catus%2529.jpg%3Futm_source%3Dcommons.wikimedia.org%26utm_campaign%3Dimageinfo%26utm_content%3Dthumbnail&w=500&fit=cover', 128, true);
 
 
 --
@@ -304,8 +290,8 @@ INSERT INTO "public"."images" ("id", "url", "animal_id", "is_primary") VALUES
 --
 
 INSERT INTO "storage"."buckets" ("id", "name", "owner", "created_at", "updated_at", "public", "avif_autodetection", "file_size_limit", "allowed_mime_types", "owner_id", "type") VALUES
-	('images', 'images', NULL, '2026-05-03 06:30:08.71652+00', '2026-05-03 06:30:08.71652+00', true, false, NULL, NULL, NULL, 'STANDARD');
-
+	('images', 'images', NULL, '2026-05-03 06:30:08.71652+00', '2026-05-03 06:30:08.71652+00', true, false, NULL, NULL, NULL, 'STANDARD')
+    ON CONFLICT (id) DO NOTHING;
 
 --
 -- Data for Name: buckets_analytics; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
@@ -328,8 +314,8 @@ INSERT INTO "storage"."objects" ("id", "bucket_id", "name", "owner", "created_at
 	('6b879bc2-bda4-4db4-a550-2544023faa42', 'images', 'IMG_2647.jpg', NULL, '2026-05-03 08:20:39.445636+00', '2026-05-03 08:20:39.445636+00', '2026-05-03 08:20:39.445636+00', '{"eTag": "\"338a94e3eb8839b284393f96a3ed9c31-1\"", "size": 641180, "mimetype": "image/jpeg", "cacheControl": "max-age=3600", "lastModified": "2026-05-03T08:20:39.000Z", "contentLength": 641180, "httpStatusCode": 200}', '1655a83c-b1dd-4a36-bfa9-d62c692fbbdf', NULL, NULL),
 	('d5a81ff1-87b2-4aba-8f89-6d54d4c789dc', 'images', 'best-guard-dogs-1650302456.avif', NULL, '2026-05-03 08:33:33.555026+00', '2026-05-03 08:33:33.555026+00', '2026-05-03 08:33:33.555026+00', '{"eTag": "\"ef232306812ea50014ea268a8271f93a-1\"", "size": 115576, "mimetype": "image/avif", "cacheControl": "max-age=3600", "lastModified": "2026-05-03T08:33:34.000Z", "contentLength": 115576, "httpStatusCode": 200}', 'f006bd6f-3662-41e8-8c67-3afae47ee3dc', NULL, NULL),
 	('3272ec7b-dc8b-4b4c-aa59-64b67108ebb2', 'images', 'dog-puppy-on-garden-royalty-free-image-1586966191.avif', NULL, '2026-05-03 08:35:52.789255+00', '2026-05-03 08:35:52.789255+00', '2026-05-03 08:35:52.789255+00', '{"eTag": "\"1c00d719b78a6715475e18f0e3807b6c-1\"", "size": 103543, "mimetype": "image/avif", "cacheControl": "max-age=3600", "lastModified": "2026-05-03T08:35:53.000Z", "contentLength": 103543, "httpStatusCode": 200}', '5599a389-7e05-48c3-8e51-ab04de9e52ee', NULL, NULL),
-	('73263828-0e4d-4faa-b918-81bc0afca41b', 'images', 'cute-puppy-pomeranian-mixed-breed-pekingese-dog-run-on-the-grass-with-happiness-photo.jpg', NULL, '2026-05-03 08:35:58.768249+00', '2026-05-03 08:35:58.768249+00', '2026-05-03 08:35:58.768249+00', '{"eTag": "\"0aa5a09d275520cfe2c1b929e439fdd8-1\"", "size": 23681, "mimetype": "image/jpeg", "cacheControl": "max-age=3600", "lastModified": "2026-05-03T08:35:59.000Z", "contentLength": 23681, "httpStatusCode": 200}', 'dde50ee1-23e8-47c4-81ac-096600a799a6', NULL, NULL);
-
+	('73263828-0e4d-4faa-b918-81bc0afca41b', 'images', 'cute-puppy-pomeranian-mixed-breed-pekingese-dog-run-on-the-grass-with-happiness-photo.jpg', NULL, '2026-05-03 08:35:58.768249+00', '2026-05-03 08:35:58.768249+00', '2026-05-03 08:35:58.768249+00', '{"eTag": "\"0aa5a09d275520cfe2c1b929e439fdd8-1\"", "size": 23681, "mimetype": "image/jpeg", "cacheControl": "max-age=3600", "lastModified": "2026-05-03T08:35:59.000Z", "contentLength": 23681, "httpStatusCode": 200}', 'dde50ee1-23e8-47c4-81ac-096600a799a6', NULL, NULL)
+    ON CONFLICT (id) DO NOTHING;
 
 --
 -- Data for Name: s3_multipart_uploads; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
